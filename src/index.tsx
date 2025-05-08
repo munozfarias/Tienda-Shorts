47641daad0b7434c6676d8810741c0f206285544
munozfarias <marcelomunozfarias>
2025-05-07 20:03:19 -0400

    Se sube avance de proyecto

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App'; // Asegúrate de que App esté exportado correctamente
 import './index.css'; // Esto importa tus estilos de Tailwind
 import { CarritoProvider } from './context/CarritoContext'; // Importa el CarritoProvider
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       {/* Envolver App con CarritoProvider */}
       <CarritoProvider>
         <App />
       </CarritoProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
